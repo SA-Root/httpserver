df9df36948110118e07a0f5c447f2e062af8ddeb
SA-Root <a124000@outlook.com>
2021-10-20 13:38:40 +0800

    add Request

---
+++ src/main/java/request/Request.java
@@ -0,0 +1,82 @@
+package request;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Request {
+    private boolean err = false;
+    private String method;
+    private String resource;
+    private final Map<String, String> headers;
+    private String postData;
+
+    private Request() {
+        headers = new HashMap<>();
+    }
+
+    public boolean isErr() {
+        return err;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public String getPostData() {
+        return postData;
+    }
+
+    public String get(String key) {
+        if (!headers.containsKey(key)) {
+            return "";
+        }
+        return headers.get(key);
+    }
+
+    public static Request parseFromInputStream(InputStream inputStream) throws IOException {
+        Request request = new Request();
+
+        byte[] buf = new byte[2048];
+
+        int cnt = inputStream.read(buf);
+
+        int i;
+
+        if (cnt > 0) {
+            String requestInfo = new String(buf);
+
+            System.out.println(requestInfo);
+
+            List<String> lines = requestInfo.lines().collect(Collectors.toList());
+
+            String[] tmp = lines.get(0).split(" ");
+
+            request.method = tmp[0];
+            request.resource = tmp[1];
+
+            for (i = 1; i < lines.size(); i++) {
+                if (lines.get(i).length() != 0) {
+                    String[] pair = lines.get(i).split(":", 2);
+                    request.headers.put(pair[0], pair[1].trim());
+                } else
+                    break;
+            }
+
+            request.postData = lines.get(i + 1);
+
+            return request;
+        }
+
+        request.err = true;
+        return request;
+    }
+
+}
--- src/main/java/response/static_resopnse.java
+++ src/main/java/response/static_resopnse.java
@@ -1,7 +1,7 @@
 package response;
 
 import request.Request;
-import builtin.SpecialPages;
+// import builtin.SpecialPages;
 import java.io.*;
 
 import java.io.File;
@@ -9,17 +9,18 @@ import java.io.FileInputStream;
 
 public class static_resopnse {
     public static Response response(Request request) throws IOException {
-        String resource_name = request.getResource();
-        Response response = new Response();
-        // 下面开始对response进行填充
-        if (resource_name.equals("/"))// 默认页面传入参数0即index.html
+        String resource_name=request.getResource();
+        Response response=new Response();
+        //下面开始对response进行填充
+        if(resource_name.equals("/"))//默认页面传入参数0即index.html
         {
-            response.setContent(SpecialPages.getPage(0));
+            // response.setContent(SpecialPages.getPage(0));
             response.setStatusCode(200);
-        } else {
-            String address = "webroot/" + resource_name;// 根据资源名进行搜索
-            File file = new File(address);
-            if (file.exists())// 文件存在则使用file读取并设定状态码
+        }
+        else {
+            String address="webroot/"+resource_name;//根据资源名进行搜索
+            File file=new File(address);
+            if(file.exists())//文件存在则使用file读取并设定状态码
             {
                 String encoding = "UTF-8";
                 Long filelength = file.length();
@@ -27,18 +28,19 @@ public class static_resopnse {
                 FileInputStream in = new FileInputStream(file);
                 in.read(filecontent);
                 in.close();
-                response.setContent(new String(filecontent, encoding));
+               response.setContent(new String(filecontent, encoding)) ;
                 response.setStatusCode(200);
-            } else// 不存在则返回404页面
+            }
+            else//不存在则返回404页面
             {
                 String encoding = "UTF-8";
-                File file1 = new File("webroot/builtin/404.html");
+                File file1=new File("webroot/builtin/404.html");
                 Long filelength = file1.length();
                 byte[] filecontent = new byte[filelength.intValue()];
                 FileInputStream in = new FileInputStream(file1);
                 in.read(filecontent);
                 in.close();
-                response.setContent(new String(filecontent, encoding));
+                response.setContent(new String(filecontent, encoding)) ;
                 response.setStatusCode(404);
             }
         }
